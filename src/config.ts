0617695f0adc81fb9c0afd820c7d64d88d2cac8f
DN5S <me@dn5s.dev>
2025-09-09 18:02:32 +0900

    Update favicon assets and site configuration

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "DN5S-DB",
-	subtitle: "DN5S Personal Blog",
+	title: "DN5S",
+	subtitle: "DB",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 125, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -31,11 +31,11 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+		  src: '/favicon/favicon-32x32.png',    // Path of the favicon, relative to the /public directory
+		  // theme: 'light',                    // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		  sizes: '32x32',                       // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		}
 	],
 };
 
